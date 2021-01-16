b51e1eb9cdc814214e6c397721e6bf6a1df837a4
FGacheru <francisgacheru2001@gmail.com>
2021-01-17 02:39:45 +0300

    add Image class

--- ig_app/models.py
+++ ig_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length =30)
+    image = CloudinaryField('image')
+    posted_date= models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length =200)
+    
