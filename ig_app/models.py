914991244ed04e3090262e30554dc8b2608ef04a
FGacheru <francisgacheru2001@gmail.com>
2021-01-17 02:47:42 +0300

    add count functionality for pple following

--- ig_app/models.py
+++ ig_app/models.py
@@ -20,6 +20,14 @@ class Image(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    @property
+    def following(self):
+        return Follow.objects.filter(user=self.user).count()
+
 
 
     
