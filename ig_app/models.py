719f653626a799ece8fcdc020c8b081ca521fe0a
FGacheru <francisgacheru2001@gmail.com>
2021-01-17 02:58:00 +0300

    add a function to save image

--- ig_app/models.py
+++ ig_app/models.py
@@ -27,6 +27,21 @@ class Profile(models.Model):
     @property
     def following(self):
         return Follow.objects.filter(user=self.user).count()
+    
+    @property
+    def followers(self):
+        return Follow.objects.filter(follow_user=self.user).count()
+    
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
 
 
