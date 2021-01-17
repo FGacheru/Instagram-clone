1b253d3de97d51cfaf0a1b75befa1ae0d466f6ac
FGacheru <francisgacheru2001@gmail.com>
2021-01-17 12:08:24 +0300

    add preference class in the models file

--- ig_app/models.py
+++ ig_app/models.py
@@ -70,6 +70,19 @@ class Comment(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post_connected = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    
+class Preference(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    post= models.ForeignKey(Post, on_delete=models.CASCADE)
+    value= models.IntegerField()
+    date= models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return str(self.user) + ':' + str(self.post) +':' + str(self.value)
+
+    class Meta:
+       unique_together = ("user", "post", "value")
 
 
 
