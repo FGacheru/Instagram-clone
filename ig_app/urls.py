6179721580ceb825d266394f48b797d5266511d0
FGacheru <francisgacheru2001@gmail.com>
2021-01-17 23:24:10 +0300

    add path for search functionality

--- ig_app/urls.py
+++ ig_app/urls.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from django.contrib.auth import views 
+from . import views
+# from django.contrib.auth import views 
 
 urlpatterns = [
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    path(r'',views.index,name='index'),
+    path('search/', views.search_results, name='search_results'),
 ] 
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
