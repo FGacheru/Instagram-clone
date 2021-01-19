a8af9f1e47f911e8c2b38d21b3320835a41d849c
FGacheru <francisgacheru2001@gmail.com>
2021-01-19 22:50:57 +0300

    add profile update form

---
+++ ig_app/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Image
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['pub_date']
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+    
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+        
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
