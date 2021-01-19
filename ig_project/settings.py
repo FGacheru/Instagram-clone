9b6ff333ddc5b2baeb42aff262fd3b84ab1c49c5
FGacheru <francisgacheru2001@gmail.com>
2021-01-20 02:03:44 +0300

    Deployment to heroku

--- ig_project/settings.py
+++ ig_project/settings.py
@@ -20,10 +20,6 @@ import dj_database_url
 from decouple import config,Csv
 
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
