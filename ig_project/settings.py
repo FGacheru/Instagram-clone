ffa8e9f32fb2de522b459434bd06aaca33eff493
FGacheru <francisgacheru2001@gmail.com>
2021-01-20 01:19:13 +0300

    Deployment to heroku

--- ig_project/settings.py
+++ ig_project/settings.py
@@ -111,11 +111,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ig_project.wsgi.application'
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Database
